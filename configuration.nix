15221799b13ecd67d67a27a7ae1d391a4e5c9407
Your Name <you@example.com>
2023-01-15 17:14:32 +0100

    using modules from linux pc

--- configuration.nix
+++ configuration.nix
@@ -27,6 +27,35 @@
 
   system.stateVersion = "22.11";
 
-
+  #  services.tailscale.enable = true;
+  #  services.tailscale.port = 12345;
+  #
+  #  systemd.services.tailscale-autoconnect = {
+  #    description = "Automatic connection to Tailscale";
+  #
+  #    # make sure tailscale is running before trying to connect to tailscale
+  #    after = [ "network-pre.target" "tailscale.service" ];
+  #    wants = [ "network-pre.target" "tailscale.service" ];
+  #    wantedBy = [ "multi-user.target" ];
+  #
+  #    # set this service as a oneshot job
+  #    serviceConfig.Type = "oneshot";
+  #
+  #    # have the job run this shell script
+  #    script = with pkgs; ''
+  #      # wait for tailscaled to settle
+  #      sleep 2
+  #
+  #      # check if we are already authenticated to tailscale
+  #      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+  #      if [ $status = "Running" ]; then # if so, then do nothing
+  #        exit 0
+  #      fi
+  #
+  #      # otherwise authenticate with tailscale
+  #      ${tailscale}/bin/tailscale up -authkey xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+  #      # ${tailscale}/bin/tailscale up -authkey tskey-kNoFRv2CNTRL-6XQhDpk6odhiMUimcQYhf
+  #    '';
+  #  };
 
 }
--- flake.nix
+++ flake.nix
@@ -3,14 +3,15 @@
   inputs = {
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    #my-nixos.url = "git+file:///home/nixos/git/nixos";
+    my-nixos.url = "github:marnyg/nixos/wsl";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-wsl }: {
-    #nixosModules.wsl = {
-    tst.moduels = {
-      imports = nixos-wsl;
-    };
-
+  outputs = { self, nixpkgs, nixos-wsl, my-nixos, home-manager }: {
     nixosConfigurations.mysystem = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {
@@ -18,12 +19,64 @@
       };
       modules = [
         ./configuration.nix
+
+        #import system module
+        my-nixos.nixosModules
+        {
+          #enable system module
+          modules.syncthing.enable = true;
+          #modules.tailscale-autoconnect.enable =true;
+        }
+
+        #import home manager
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          #home-manager.extraSpecialArgs = { inherit username; };
+          #home-manager.users.nixos.imports = [ (import ./home.nix) ] ++ [ (import ./madoka/home.nix) ];
+          home-manager.users.nixos.imports = [
+            #import home manager modules
+            #  { config, lib, pkgs, username, ... }:
+            {
+              programs.home-manager.enable = true;
+
+              programs.keychain.enable = true;
+              programs.keychain.enableZshIntegration = true;
+
+              programs.direnv.enable = true;
+              programs.direnv.enableZshIntegration = true;
+
+              services.lorri.enable = true;
+
+              home = {
+                username = "nixos";
+                homeDirectory = "/home/nixos";
+                stateVersion = "22.05";
+
+                file.".config/nixpkgs/config.nix" = {
+                  text = ''
+                    { allowUnfree = true; }
+                  '';
+                };
+
+                #sessionPath = [
+                #  "$HOME/go/bin"
+                #  "$HOME/.local/bin"
+                #  "$HOME/bin"
+                #];
+              };
+            }
+            my-nixos.homeManagerModules
+            {
+              #modules.firefox.enable = true;
+              modules.zsh.enable = true;
+              modules.dunst.enable = true;
+              # enable home manager modules
+            }
+          ];
+        }
       ];
     };
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
   };
 }
